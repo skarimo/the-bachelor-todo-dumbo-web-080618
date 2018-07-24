631d06e882f59f94ed1e4302575a1c82c5b41608
Sherzod Karimov <karimovj@gmail.com>
2018-07-24 05:04:49 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,78 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].each do |people|
+
+     people.each do |k,v|
+      if v == "Winner"
+          name = people["name"]
+          return name.split(" ").first
+   end 
+  end
+ end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+ data.each do |season, x|
+    x.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+   end
+  end
+ end
+end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0 
+    data.each do |season, x|
+      x.each do |people|
+        people.each do |k, v|
+          if v == hometown
+            count += 1 
+   end
+  end
+end
+end
+count
 end
 
 def get_occupation(data, hometown)
-  # code here
+data.each do |season, x|
+      x.each do |people|
+        people.each do |k, v|
+          if v == hometown
+           return people["occupation"]
+   end
+  end
+end
+end
+end
+
+def average(array)
+  age_sum = array.reduce(:+)
+  total = array.length 
+  
+  ans = (age_sum/total)
+  ans.round
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+    data.each do |season_t, array|
+      if season_t == season 
+        array.each do |people|
+          people.each do |k, v|
+            ages << people["age"].to_f
+        end
+      end
+    end
+    
+  end
+average(ages)
 end
+
+
+
+
